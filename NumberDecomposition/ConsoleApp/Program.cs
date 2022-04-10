bc89b3ebb2812c3ec96c9cca16a83f417f5679fa
JOÃO PAULO SOARES LESSA <jpaulo.lessa@gmail.com>
2022-04-10 14:14:55 -0300

    Estrutura do Projeto

---
+++ NumberDecomposition/ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp
+{
+using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
